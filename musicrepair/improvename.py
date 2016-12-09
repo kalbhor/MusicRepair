c18f1d734b25497677e7fedfc5335cd2de5ed950
Moyiz <moyiz@users.noreply.github.com>
2016-12-09 18:14:59 +0200

    Improved improvename
    Replacing the repls dict with a list of characters to replace with space, and a list of words (case insensitive) to remove
    Improve extracting filename without the extension
    Remove duplicate and unnecessary space

--- musicrepair/improvename.py
+++ musicrepair/improvename.py
@@ -1,43 +1,28 @@
 import re
+from os.path import splitext
+
 
 def songname(song_name):
     '''
     Improves file name by removing crap words
     '''
+    try:
+        song_name = splitext(song_name)[0]
+    except IndexError:
+        pass
 
-    song_name = song_name[:-4]
+    # Words to omit from song title for better results through spotify's API
+    chars_filter = "()[]{}-:_/=+\"\'"
+    words_filter = ('official', 'lyrics', 'audio', 'remixed', 'remix', 'video',
+                    'full', 'version', 'music', 'mp3', 'hd', 'hq', 'uploaded')
 
-    repls = {  # Words to omit from song title for better results through spotify's API
-        '(':"", ')':"",
+    # Replace characters to filter with spaces
+    song_name = ''.join(map(lambda c: " " if c in chars_filter else c, song_name))
 
-        '[':"", ']':"",
+    # Remove crap words
+    song_name = re.sub('|'.join(re.escape(key) for key in words_filter),
+                       "", song_name, flags=re.IGNORECASE)
 
-        '{':"", '}':"",
-
-        'official': "", 'Official': "",
-
-        'lyrics': "", 'Lyrics': "",
-
-        'audio': "", 'Audio': "",
-
-        'remix': "", 'Remix': "",
-
-        'video':"", 'Video':"",
-
-        'full':"", 'Full':"",
-
-        'version':"", 'Version':"",
-
-        'music':"", 'Music':"",
-
-        '.mp3':"",
-
-        'HD':"", 'hd':"",
-
-        'hq':"", 'HQ':"",
-    }
-
-    song_name = re.sub('|'.join(re.escape(key) for key in repls.keys()),
-                       lambda k: repls[k.group(0)], song_name)  # Regex to substitute repls
-
-    return song_name
+    # Remove duplicate spaces
+    song_name = re.sub(' +', ' ', song_name)
+    return song_name.strip()
