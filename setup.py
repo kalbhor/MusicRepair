cbc2e008a6b2e50f6f1a32ff6e34de38ed9c2929
Brandon Brown <brandon@brandonb.io>
2016-12-10 21:43:09 -0800

    Format setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
 from setuptools import setup
 
-setup(name='musicrepair',
-      version='4.3',
-      description='Lets you repair your music files by adding metadata and album art',
-      url='https://github.com/lakshaykalbhor/musicrepair',
-      author='Lakshay Kalbhor',
-      author_email='lakshaykalbhor@gmail.com',
-      license='MIT',
-      packages=['musicrepair'],
-      install_requires=[
-          'bs4',
-          'mutagen',
-          'spotipy',
-          'six',
-      ],
-      entry_points={
-          'console_scripts': ['musicrepair=musicrepair.command_line:main'],
-      },
-      )
+setup(
+    name='musicrepair',
+    version='4.3',
+    description='Lets you repair your music files by adding metadata and album art',
+    url='https://github.com/lakshaykalbhor/musicrepair',
+    author='Lakshay Kalbhor',
+    author_email='lakshaykalbhor@gmail.com',
+    license='MIT',
+    packages=['musicrepair'],
+    install_requires=[
+        'bs4',
+        'colorama',
+        'mutagen',
+        'spotipy',
+        'six',
+    ],
+    entry_points={
+        'console_scripts': ['musicrepair=musicrepair.command_line:main'],
+    },
+)
